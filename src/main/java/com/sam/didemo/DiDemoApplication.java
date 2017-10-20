4cb848c183cbaa94b7f4512517e9be6b9b4daa13
Charizard11 <baskicisamet@gmail.com>
2017-10-20 16:52:34 +0300

    Initial commit

---
+++ src/main/java/com/sam/didemo/DiDemoApplication.java
@@ -0,0 +1,16 @@
+package com.sam.didemo;
+
+import com.sam.didemo.controller.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+		MyController myController =(MyController) ctx.getBean("myController");
+		myController.hello();
+	}
+}
