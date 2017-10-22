94250818e8ebe4a56c71efa47d4058da0d0e01dd
Charizard11 <baskicisamet@gmail.com>
2017-10-22 18:37:48 +0300

    default profile

--- src/main/java/com/sam/didemo/service/PrimaryGermanGreetingService.java
+++ src/main/java/com/sam/didemo/service/PrimaryGermanGreetingService.java
@@ -6,11 +6,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Primary
-@Profile({"en","default"}) //this will be active when there is no active profile
-public class PrimaryGreetingService implements GreetingService {
+@Profile("de")
+public class PrimaryGermanGreetingService implements  GreetingService{
+
 
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting service";
+        return "primärer Begrüßungsdienst";
     }
 }
