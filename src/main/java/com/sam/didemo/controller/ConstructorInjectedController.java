3edf61c43189b84b29cd3fcb3ea462ec9df557de
Charizard11 <baskicisamet@gmail.com>
2017-10-22 14:30:44 +0300

    bean profiles

--- src/main/java/com/sam/didemo/controller/ConstructorInjectedController.java
+++ src/main/java/com/sam/didemo/controller/ConstructorInjectedController.java
@@ -1,8 +1,6 @@
 package com.sam.didemo.controller;
 
 import com.sam.didemo.service.GreetingService;
-import com.sam.didemo.service.GreetingServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
@@ -11,7 +9,7 @@ public class ConstructorInjectedController {
 
     private GreetingService greetingService;
 
-     //it is optional, we don't need to this annotation, its just for readable code
+
     public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
