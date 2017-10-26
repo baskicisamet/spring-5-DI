4299479785db73267fbbd6f1a67a19376024a81f
Charizard11 <baskicisamet@gmail.com>
2017-10-26 19:59:16 +0300

    componentScan

--- src/main/java/com/sam/didemo/DiDemoApplication.java
+++ src/main/java/com/sam/didemo/DiDemoApplication.java
@@ -7,8 +7,10 @@ import com.sam.didemo.controller.PropertyInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.sam"})
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
