4bad01d3ecfb7604b2a1dbc8e71af754525e22fe
Charizard11 <baskicisamet@gmail.com>
2017-10-30 04:04:54 +0300

    adding environment property

--- src/main/java/com/sam/didemo/config/PropertyConfig.java
+++ src/main/java/com/sam/didemo/config/PropertyConfig.java
@@ -1,15 +1,22 @@
 package com.sam.didemo.config;
 
 import com.sam.didemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    //if environment property name is same requested property, it will override it('_' character in environment equals to '.' properties java)
+
+    @Autowired
+    Environment env;
 
     @Value("${sam.username}")
     String user;
@@ -22,7 +29,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(this.user);
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(this.password);
         fakeDataSource.setUrl(this.url);
 
