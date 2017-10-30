446d00f07a036738d93fe2a32964a4f8718840d7
Charizard11 <baskicisamet@gmail.com>
2017-10-30 03:32:01 +0300

    adding property source

--- src/main/java/com/sam/didemo/DiDemoApplication.java
+++ src/main/java/com/sam/didemo/DiDemoApplication.java
@@ -4,13 +4,13 @@ import com.sam.didemo.controller.ConstructorInjectedController;
 import com.sam.didemo.controller.GetterInjectedController;
 import com.sam.didemo.controller.MyController;
 import com.sam.didemo.controller.PropertyInjectedController;
+import com.sam.didemo.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.sam"})
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
@@ -18,11 +18,10 @@ public class DiDemoApplication {
 		MyController myController =(MyController) ctx.getBean("myController");
 
 
-		// @qualifier >> @Primary >> bean name  !!!!!!!!!!!!!
+		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
 
-		System.out.println(myController.sayHello());
-		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+		System.out.println(fakeDataSource);
+
+
 	}
 }
---
+++ src/main/java/com/sam/didemo/config/PropertyConfig.java
@@ -0,0 +1,39 @@
+package com.sam.didemo.config;
+
+import com.sam.didemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    @Value("${sam.username}")
+    String user;
+
+    @Value("${sam.password}")
+    String password;
+    @Value("${sam.url}")
+    String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource(){
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(this.user);
+        fakeDataSource.setPassword(this.password);
+        fakeDataSource.setUrl(this.url);
+
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties(){
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+
+    }
+
+}
